4414e04cb627cd9adb09d4365bcefc623bc902ff
Brad Stalcup <bstalcup@nd.edu>
2014-06-25 18:26:26 -0400

    adding animations

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ window.onload = function(){
 	$('#b2').click(function(){
 		$.get('coding.json', function(data){
 			$('#content').empty().removeClass('fadeInRight animated');
+			console.log($('#content'));
 			$('#content').html(data.content).addClass('fadeInRight animated');
 		});
 	})
