b339a44d84660bdc44915a7d0d2172c86bd6ff4d
Brad Stalcup <bstalcup@nd.edu>
2014-06-25 23:23:12 -0400

    getting animation to work

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 window.onload = function(){
 	$('#b2').click(function(){
+		if($('#content h2').text() === 'Coding'){return;}
 		$.get('coding.json', function(data){
-			$('#content').empty().removeClass('fadeInRight animated');
+			$('#content ').empty().removeClass('fadeInRight animated');
 			setTimeout(function(){$('#content').html(data.content).addClass('fadeInRight animated')}, 150);
 		});
 	})
